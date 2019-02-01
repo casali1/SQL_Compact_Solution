87d716e99cd881745458152c6bb6a71a3bd77588
casali1 <frank_casali@hotmail.com>
2019-01-31 17:26:53 -0700

    Add project files.

---
+++ SQL_Compact_Solution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SQL_Compact_Solution.MvcApplication" Language="C#" %>
---
+++ SQL_Compact_Solution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SQL_Compact_Solution.Startup))]
+namespace SQL_Compact_Solution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
